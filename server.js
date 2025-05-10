ff5c4403285ee08b8ecad29de177c658b9723bd4
Kummirai <ajaxmilton@hotmail.com>
2025-05-10 17:20:17 +0200

    Add initial layout template for the application

--- server.js
+++ server.js
@@ -9,18 +9,31 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layouts") // not at views root
+
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home",
+    description: "This is the home page",
+  })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
