39db5f005ec1e3187f393e420fa05a3eb29a16ef
Kummirai <ajaxmilton@hotmail.com>
2025-05-22 23:13:21 +0200

    Implement inventory management features with classification view and navigation

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const expressLayouts = require("express-ejs-layouts")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const inventoryRoute = require("./routes/inventoryRoute");
+const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController");
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layouts") // not at views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layouts"); // not at views root
 
-
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Home",
-    description: "This is the home page",
-  })
-})
-
+app.get("/", baseController.buildHome);
+// Inventory routes
+app.use("/inv", inventoryRoute);
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500
-const host = process.env.HOST || "localhost"
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || "localhost";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
