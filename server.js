fd1def7ea4b9cc82df7f0b8dc1b0b21443da746c
Kummirai <ajaxmilton@hotmail.com>
2025-06-09 21:14:02 +0200

    Add session management and flash messaging; update dependencies and enhance index view

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const inventoryRoute = require("./routes/inventoryRoute");
 const expressLayouts = require("express-ejs-layouts");
 const baseController = require("./controllers/baseController");
 const utilities = require("./utilities/");
+const session = require("express-session");
+const pool = require("./database/");
 
 /* ***********************
  * Middleware
@@ -17,6 +19,29 @@ const utilities = require("./utilities/");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+);
+
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
 
 /* ***********************
  * View Engine Setup
